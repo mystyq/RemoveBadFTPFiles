023841a9de1847090ab7fe6e2d98255491e4067a
mystyq <26229904+mystyq@users.noreply.github.com>
2024-04-08 12:47:21 -0500

    Cleanup

--- main.c
+++ main.c
@@ -4,56 +4,65 @@
 #include <nxdk/mount.h>
 #include <windows.h>
 
-void Remove_Directory(LPCSTR lpPathName) {
-	BOOL success;
-	success = nxMountDrive('E', "\\Device\\Harddisk0\\Partition1\\");
-	if (!success) {
-        debugPrint("Failed to mount E: drive!\n");
+void Remove_Directory(LPCSTR lpPathName)
+{
+	if (!nxMountDrive('E', "\\Device\\Harddisk0\\Partition1\\"))
+	{
+		debugPrint("Failed to mount E: drive!\n");
 		return;
-    }
-	success = RemoveDirectory(lpPathName);
-	if(success) {
+	}
+	if (RemoveDirectory(lpPathName))
+	{
 		debugPrint("Directory removed successfully\n");
-	} else {
+	}
+	else
+	{
 		DWORD error = GetLastError();
-		switch(error) {
-			case ERROR_DIR_NOT_EMPTY: 
-				debugPrint("The directory is not empty."); break;
-			case ERROR_PATH_NOT_FOUND: 
-				debugPrint("The system cannot find the path specified."); break;
-			default: 
+		switch (error)
+		{
+			case ERROR_DIR_NOT_EMPTY:
+				debugPrint("The directory is not empty.");
+				break;
+			case ERROR_PATH_NOT_FOUND:
+				debugPrint("The system cannot find the path specified.");
+				break;
+			default:
 				debugPrint("error: %lx\n", error);
 		}
 	}
 }
 
-void Delete_File(LPCSTR lpFileName) {
-	BOOL success;
-	success = nxMountDrive('E', "\\Device\\Harddisk0\\Partition1\\");
-	if (!success) {
-        debugPrint("Failed to mount E: drive!\n");
+void Delete_File(LPCSTR lpFileName)
+{
+	if (!nxMountDrive('E', "\\Device\\Harddisk0\\Partition1\\"))
+	{
+		debugPrint("Failed to mount E: drive!\n");
 		return;
-    }
-	success = DeleteFile(lpFileName);
-	if(success) {
+	}
+	if (DeleteFile(lpFileName))
+	{
 		debugPrint("File deleted successfully\n");
-	} else {
+	}
+	else
+	{
 		DWORD error = GetLastError();
-		switch(error) {
-			default: 
+		switch (error)
+		{
+			default:
 				debugPrint("error: %lx\n", error);
 		}
 	}
 }
 
-int main(void) {
+int main(void)
+{
 	VIDEO_MODE xmode;
-	void* p = NULL;
+	void *p = NULL;
 	while (XVideoListModes(&xmode, 0, 0, &p)) { }
 	XVideoSetMode(xmode.width, xmode.height, xmode.bpp, xmode.refresh);
 
-	//Remove_Directory();
-	//Delete_File("E:\\Apps\\hello\\   ");
+	// Remove_Directory();
+	// Delete_File("E:\\Apps\\hello\\   ");
 
 	XReboot();
 	return 0;
